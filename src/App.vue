4c88840e93fb1c1f09fa292d13b33ed68fa53fb7
khaingzinkhaingzin <khaingthazin8july@gmail.com>
2024-04-10 23:00:35 +0630

    create component, html ref attribute, style scoped attribute and inject external css file in main.js

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>Hi</h1>
+  <Modal />
+</template>
+
+<script>
+import Modal from "./components/Modal.vue"
+export default {
+  data() {
+    return {
+      title: "You don't know JS"
+    }
+  },
+  components: {
+    Modal
+  }
+}
+</script>
+
+<style scoped>
+h1 {
+  color: blue;
+}
+</style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="backdrop">
+    <div class="modal">
+        <h1>Modal Header</h1>
+        <p>Modal Content</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+    .backdrop {
+        position: fixed;
+        top: 0;
+        background: rgba(0, 0, 0, 0.5);
+        width: 100%;
+        height: 100%;
+    }
+    .modal {
+        width: 400px;
+        padding: 20px;
+        margin: 100px auto;
+        background-color: white;
+        border-radius: 10px;
+    }
+</style>
